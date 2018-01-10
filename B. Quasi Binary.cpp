257a57031b303103db83b150cb3764686d19505f
Jonck <jonckjuniorr@gmail.com>
2018-01-10 15:32:11 -0200

    Update B. Quasi Binary.cpp

--- B. Quasi Binary.cpp
+++ B. Quasi Binary.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 typedef vector<int> vi;
 int len[1000005];
-vector<vi> dp(1000005);
+int dp[1000005];
 vi num;
 
 string s;
@@ -23,7 +23,8 @@ int main() {
     sort(num.begin(), num.end());
     len[1] = 1;
     len[0] = 0;
-    dp[1].push_back(1);
+    dp[1] = 1;
+    dp[0] = 0;
     for(int i = 2; i <= n; i++){
         len[i] = 1e9;
         int x = 0;
@@ -34,12 +35,11 @@ int main() {
                 x = num[j];
             }
         }
-        dp[i] = dp[i-x];
-        dp[i].push_back(x);
+        dp[i] = x;
     }
     cout << len[n] << endl;
-    for(int i = 0; i < dp[n].size(); i++)
-        cout << dp[n][i] << " ";
+    for(int i = n; dp[i] != 0; i = i - dp[i])
+        cout << dp[i] << " ";
     cout << endl;
     return 0;
 }
