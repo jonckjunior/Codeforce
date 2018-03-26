f3faa392e33f23317f515f482fbd1cd14d713a48
Jonck <jonckjuniorr@gmail.com>
2018-03-25 22:39:28 -0300

    Update A. Crazy Town.cpp

--- A. Crazy Town.cpp
+++ A. Crazy Town.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+typedef long long int ll;
 int main(){
-    int x, y; cin >> x >> y;
-    int px, py; cin >> px >> py;
-    int n; cin >> n;
-    int res = 0;
+    ll x, y; cin >> x >> y;
+    ll px, py; cin >> px >> py;
+    ll n; cin >> n;
+    ll res = 0;
     for(int i = 0; i < n; i++){
-        int a, b, c; cin >> a >> b >> c;
-        if(a*x + b*y + c > 0 != a*px + b*py + c > 0)
+        ll a, b, c; cin >> a >> b >> c;
+        if(1LL*a*x + b*y + c > 0 != 1LL*a*px + b*py + c > 0)
             res++;
     }
     cout << res << endl;
