cc939587cb0b7554556245a134c3d5a9f4b6ef03
Jonck <jonckjuniorr@gmail.com>
2018-04-01 23:57:02 -0300

    Create H. A + B Strikes Back.cpp

---
+++ H. A + B Strikes Back.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main(){
+    ios::sync_with_stdio(false);
+    ll a, b; cin >> a >> b;    
+    cout << a+b << endl;
+    return 0;
+}
